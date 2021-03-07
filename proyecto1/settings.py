dea44feb53cb4a646b8f5c82db46e164302554f8
Angelbitcloud <angello29@utp.edu.co>
2021-03-07 16:07:18 -0500

    last

--- proyecto1/settings.py
+++ proyecto1/settings.py
@@ -129,3 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+
+DEFAULT_AUTHENTICATION_CLASSES:(
+    'rest_framework.authentication.OAuth2Authentication',
+)
